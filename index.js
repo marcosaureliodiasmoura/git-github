7d4da3d3edf8ced218d01911f15803645753bd13
MarcosMoura <marcos_moura@live.com>
2019-06-28 08:29:17 -0300

    chore: Altera mensagem da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/teste", (req, res) => {
-  return res.json({ hello: "Mundo" });
+  return res.json({ hello: "Mundo novo" });
 });
 
 app.listen(3333);
